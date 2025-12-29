8d30641eef5ed3307ec50cee3eb54314a0a85403
Kalki007-G <re53289@gmail.com>
2025-12-29 22:56:11 +0530

    Move checkout  page into React

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -2,47 +2,48 @@ import './header.css';
 import './HomePage.css';
 
 
-export function HomePage(){
-    return(
-    <>
-        <div className="header">
-                    <div className="left-section">
-                        <a href="index.html" className="header-link">
+export function HomePage() {
+    return (
+        <>
+            <title>Ecommerce Project</title>
+            <div className="header">
+                <div className="left-section">
+                    <a href="index.html" className="header-link">
                         <img className="logo"
                             src="images/logo-white.png" />
                         <img className="mobile-logo"
                             src="images/mobile-logo-white.png" />
-                        </a>
-                    </div>
+                    </a>
+                </div>
 
-                    <div className="middle-section">
-                        <input className="search-bar" type="text" placeholder="Search" />
+                <div className="middle-section">
+                    <input className="search-bar" type="text" placeholder="Search" />
 
-                        <button className="search-button">
+                    <button className="search-button">
                         <img className="search-icon" src="images/icons/search-icon.png" />
-                        </button>
-                    </div>
+                    </button>
+                </div>
 
-                    <div className="right-section">
-                        <a className="orders-link header-link" href="orders.html">
+                <div className="right-section">
+                    <a className="orders-link header-link" href="orders.html">
 
                         <span className="orders-text">Orders</span>
-                        </a>
+                    </a>
 
-                        <a className="cart-link header-link" href="checkout.html">
+                    <a className="cart-link header-link" href="checkout.html">
                         <img className="cart-icon" src="images/icons/cart-icon.png" />
                         <div className="cart-quantity">3</div>
                         <div className="cart-text">Cart</div>
-                        </a>
-                    </div>
-        </div>
+                    </a>
+                </div>
+            </div>
 
-        <div className="home-page">
-                    <div className="products-grid">
-                        <div className="product-container">
+            <div className="home-page">
+                <div className="products-grid">
+                    <div className="product-container">
                         <div className="product-image-container">
                             <img className="product-image"
-                            src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+                                src="images/products/athletic-cotton-socks-6-pairs.jpg" />
                         </div>
 
                         <div className="product-name limit-text-to-2-lines">
@@ -51,9 +52,9 @@ export function HomePage(){
 
                         <div className="product-rating-container">
                             <img className="product-rating-stars"
-                            src="images/ratings/rating-45.png" />
+                                src="images/ratings/rating-45.png" />
                             <div className="product-rating-count link-primary">
-                            87
+                                87
                             </div>
                         </div>
 
@@ -63,16 +64,16 @@ export function HomePage(){
 
                         <div className="product-quantity-container">
                             <select>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
-                            <option value="8">8</option>
-                            <option value="9">9</option>
-                            <option value="10">10</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
                             </select>
                         </div>
 
@@ -86,12 +87,12 @@ export function HomePage(){
                         <button className="add-to-cart-button button-primary">
                             Add to Cart
                         </button>
-                        </div>
+                    </div>
 
-                        <div className="product-container">
+                    <div className="product-container">
                         <div className="product-image-container">
                             <img className="product-image"
-                            src="images/products/intermediate-composite-basketball.jpg" />
+                                src="images/products/intermediate-composite-basketball.jpg" />
                         </div>
 
                         <div className="product-name limit-text-to-2-lines">
@@ -100,9 +101,9 @@ export function HomePage(){
 
                         <div className="product-rating-container">
                             <img className="product-rating-stars"
-                            src="images/ratings/rating-40.png" />
+                                src="images/ratings/rating-40.png" />
                             <div className="product-rating-count link-primary">
-                            127
+                                127
                             </div>
                         </div>
 
@@ -112,16 +113,16 @@ export function HomePage(){
 
                         <div className="product-quantity-container">
                             <select>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
-                            <option value="8">8</option>
-                            <option value="9">9</option>
-                            <option value="10">10</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
                             </select>
                         </div>
 
@@ -135,12 +136,12 @@ export function HomePage(){
                         <button className="add-to-cart-button button-primary">
                             Add to Cart
                         </button>
-                        </div>
+                    </div>
 
-                        <div className="product-container">
+                    <div className="product-container">
                         <div className="product-image-container">
                             <img className="product-image"
-                            src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" />
+                                src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" />
                         </div>
 
                         <div className="product-name limit-text-to-2-lines">
@@ -149,9 +150,9 @@ export function HomePage(){
 
                         <div className="product-rating-container">
                             <img className="product-rating-stars"
-                            src="images/ratings/rating-45.png" />
+                                src="images/ratings/rating-45.png" />
                             <div className="product-rating-count link-primary">
-                            56
+                                56
                             </div>
                         </div>
 
@@ -161,16 +162,16 @@ export function HomePage(){
 
                         <div className="product-quantity-container">
                             <select>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
-                            <option value="8">8</option>
-                            <option value="9">9</option>
-                            <option value="10">10</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
                             </select>
                         </div>
 
@@ -184,10 +185,10 @@ export function HomePage(){
                         <button className="add-to-cart-button button-primary">
                             Add to Cart
                         </button>
-                        </div>
                     </div>
-        </div>
-    </>
+                </div>
+            </div>
+        </>
 
 
     );
